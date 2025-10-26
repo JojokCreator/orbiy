27be4e0bf405c983b03d8992e60acb38f4560838
unknown <joeyk2k@gmail.com>
2025-10-26 17:35:11 +0900

    types

--- src/config/site.ts
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  title: "ORBiY",
-  description: "Premium automotive solutions and services",
-  url: "https://example.com",
-  ogImage: "/images/og-default.jpg",
-  author: "ORBiY Team",
-};
-
-export type SiteConfig = typeof siteConfig;
