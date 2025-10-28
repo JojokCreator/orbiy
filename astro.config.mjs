11a0dace6f4a0ac8c023feac0505e7cf930ba583
unknown <joeyk2k@gmail.com>
2025-10-28 12:28:13 +0900

    hero videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig({
             block: "storyblok/HeroVideo",
             block: "storyblok/Features",
             block: "storyblok/FeaturedCars",
+            block: "storyblok/AllCars",
             block: "storyblok/FaqSection",
             block: "storyblok/ImageGrid",
             block: "storyblok/ImageText",
@@ -46,11 +47,36 @@ export default defineConfig({
 	},
   output: isDev ? 'server' : 'static',
 	experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "Poppins",
-            cssVariable: "--font-poppins",
-            weights:["100 800"]
-        }]
-    }
+		fonts: [
+			{
+				provider: "local",
+				name: "Solomon Sans",
+				cssVariable: "--font-heading",
+				variants: [
+					{
+						weight: 400,
+						style: "normal",
+						src: ["./src/assets/fonts/SolomonSans-Medium.woff2"]
+					},
+					{
+						weight: 500,
+						style: "semi-bold",
+						src: ["./src/assets/fonts/SolomonSans-SemiBold.woff2"]
+					},
+				]
+			},
+			{
+				provider: "local",
+				name: "TT Interphases Pro",
+				cssVariable: "--font-sans",
+				variants: [
+					{
+						weight: "100 900",
+						style: "normal",
+						src: ["./src/assets/fonts/TT-Interphases-Pro-Trl-Variable.woff2"]
+					}
+				]
+			}
+		]
+	}
 });
