316412293a86e5e2113ef6f061debad799269597
unknown <joeyk2k@gmail.com>
2025-10-28 18:27:53 +0900

    preview env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly STORYBLOK_PREVIEW: string;
+    readonly STORYBLOK_LOCAL: string;
+    readonly STORYBLOK_TOKEN: string;
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
