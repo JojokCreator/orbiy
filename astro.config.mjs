c1e165e164989a32b6b92a06ff52f59a01d9d31c
unknown <joeyk2k@gmail.com>
2025-10-28 13:50:26 +0900

    hero videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,77 +6,86 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import mkcert from "vite-plugin-mkcert";
 
+import netlify from "@astrojs/netlify";
+
 const env = loadEnv(process.env.NODE_ENV || "", process.cwd(), 'STORYBLOK');
 const isDev = import.meta.env.DEV;
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	image: {
-		domains: ['a.storyblok.com'],
+  site: "https://example.com",
+
+  image: {
+      domains: ['a.storyblok.com'],
 	},
-	integrations: [icon(), sitemap(),
-        storyblok({
-        accessToken: env.STORYBLOK_TOKEN,
-        livePreview: isDev ? true : false,
-        components: {
-            block: "storyblok/Hero",
-            block: "storyblok/HeroVideo",
-            block: "storyblok/Features",
-            block: "storyblok/FeaturedCars",
-            block: "storyblok/AllCars",
-            block: "storyblok/FaqSection",
-            block: "storyblok/ImageGrid",
-            block: "storyblok/ImageText",
-            block: "storyblok/Process",
-            block: "storyblok/Contact",
-            block: "storyblok/CountDown",
-            countdown_feature: "storyblok/CountDownFeature",
-        },
-        enableFallbackComponent: isDev ? true : false,
-        customFallbackComponent: 'storyblok/FallbackComponent',
-        apiOptions: {
-          region: 'eu',
-        },
-      })],
-	vite: {
-    server: isDev ? {
-      https: true,
-    } : undefined,
-		plugins: [tailwindcss(), mkcert()],
+
+  integrations: [icon(), sitemap(),
+      storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      livePreview: isDev ? true : false,
+      components: {
+          block: "storyblok/Hero",
+          block: "storyblok/HeroVideo",
+          block: "storyblok/Features",
+          block: "storyblok/FeaturedCars",
+          block: "storyblok/AllCars",
+          block: "storyblok/FaqSection",
+          block: "storyblok/ImageGrid",
+          block: "storyblok/ImageText",
+          block: "storyblok/Process",
+          block: "storyblok/Contact",
+          block: "storyblok/CountDown",
+          countdown_feature: "storyblok/CountDownFeature",
+      },
+      enableFallbackComponent: isDev ? true : false,
+      customFallbackComponent: 'storyblok/FallbackComponent',
+      apiOptions: {
+        region: 'eu',
+      },
+    })],
+
+  vite: {
+  server: isDev ? {
+    https: true,
+  } : undefined,
+      plugins: [tailwindcss(), mkcert()],
 	},
+
   output: isDev ? 'server' : 'static',
-	experimental: {
-		fonts: [
-			{
-				provider: "local",
-				name: "Solomon Sans",
-				cssVariable: "--font-heading",
-				variants: [
-					{
-						weight: 400,
-						style: "normal",
-						src: ["./src/assets/fonts/SolomonSans-Medium.woff2"]
-					},
-					{
-						weight: 500,
-						style: "normal",
-						src: ["./src/assets/fonts/SolomonSans-SemiBold.woff2"]
-					},
-				]
-			},
-			{
-				provider: "local",
-				name: "TT Interphases Pro",
-				cssVariable: "--font-sans",
-				variants: [
-					{
-						weight: "100 900",
-						style: "normal",
-						src: ["./src/assets/fonts/TT-Interphases-Pro-Trl-Variable.woff2"]
-					}
-				]
-			}
-		]
-	}
+
+  experimental: {
+      fonts: [
+          {
+              provider: "local",
+              name: "Solomon Sans",
+              cssVariable: "--font-heading",
+              variants: [
+                  {
+                      weight: 400,
+                      style: "normal",
+                      src: ["./src/assets/fonts/SolomonSans-Medium.woff2"]
+                  },
+                  {
+                      weight: 500,
+                      style: "normal",
+                      src: ["./src/assets/fonts/SolomonSans-SemiBold.woff2"]
+                  },
+              ]
+          },
+          {
+              provider: "local",
+              name: "TT Interphases Pro",
+              cssVariable: "--font-sans",
+              variants: [
+                  {
+                      weight: "100 900",
+                      style: "normal",
+                      src: ["./src/assets/fonts/TT-Interphases-Pro-Trl-Variable.woff2"]
+                  }
+              ]
+          }
+      ]
+	},
+
+  adapter: netlify()
 });
